16e19b42f85544128317488bb6400bdb9e778dbb
Yiting Shen <shenyiting2018@gmail.com>
2022-07-09 22:08:53 -0700

    add list_namespaces

--- k8s_lib/k8s_lib.go
+++ k8s_lib/k8s_lib.go
@@ -1,1 +1,42 @@
 package k8s_lib
+
+import (
+	"context"
+	"flag"
+	"path/filepath"
+
+	apiv1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+)
+
+func connect() *kubernetes.Clientset {
+	var kubeconfig *string
+	home := homedir.HomeDir()
+	kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "")
+	flag.Parse()
+
+	// use the current context in kubeconfig
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// create the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+	return clientset
+}
+
+func ListNameSpaces() *apiv1.NamespaceList {
+	clientset := connect()
+	namespaces, err := clientset.CoreV1().Namespaces().List(context.TODO(), metav1.ListOptions{})
+	if err != nil {
+		panic(err.Error())
+	}
+	return namespaces
+}
